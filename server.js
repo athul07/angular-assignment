0cdb1fb45e0dcd4cadc3c8c53fac5630e53c41b5
Athul <athulk@qburst.com>
2020-04-13 17:09:46 +0530

    change PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
